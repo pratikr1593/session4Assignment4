84db95c5ac64212cfb851994afce7803d22cfa8a
Pratik Ratnaparkhi <pratikr557@gmail.com>
2016-06-08 22:44:36 +0530

    Add files via upload

---
+++ Session4Assignment4.java
@@ -0,0 +1,13 @@
+
+public class Session4Assignment4 {
+
+//Print reverse of a given String value. 
+	
+   public static void main(String[] args){
+      String string="Pratik";
+      String reverse = new StringBuffer(string).
+      reverse().toString();
+      System.out.println("\nString before reverse: "+string);
+      System.out.println("String after reverse:"+reverse);
+   }
+}
